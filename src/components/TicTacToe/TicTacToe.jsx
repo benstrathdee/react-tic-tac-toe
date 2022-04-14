3b9b569cd2b10fac5b79e2c8c3ff85cb1a9d7146
Ben Strathdee <ben.strathdee@live.com.au>
2022-04-14 15:20:32 +1000

    refactored to cut down code length, remove unnecessary states, useRef instead of id

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,123 +1,114 @@
 import React from "react";
-import { useState } from "react";
-import styles from "./TicTacToe.module.css";
+import { useState, useRef } from "react";
+import s from "./TicTacToe.module.css";
 
-const TicTacToe = ({ size = 300 }) => {
-	const [infoOpacity, setInfoOpacity] = useState(1);
-	const [infoZIndex, setInfoZIndex] = useState(1);
-	const [infoText, setInfoText] = useState("New Game?");
-	const [previewSquare, setPreviewSquare] = useState("");
-	const [filledSquares, setFilledSquares] = useState([]);
-
-	// These values can be whatever you want
-	// (although ideally just one character)
-	const playerOne = "○";
-	const playerTwo = "✖";
+const TicTacToe = ({ size = 300, playerOne = "○", playerTwo = "✖" }) => {
+	const [infoState, setInfoState] = useState(1); // controls info visibility
+	const [infoText, setInfoText] = useState("New Game?"); // info content
+	const [filledSquares, setFilledSquares] = useState([]); // board status
+	const board = useRef(); // board reference
 
 	// These values can be any valid CSS color
-	const winnerColor = "lightgreen";
-	const loserColor = "pink";
-	const playerColor = "black";
-	const previewColor = "lightgray";
+	const winnerColor = "lightgreen"; // square colour of winner
+	const loserColor = "pink"; // square colour of loser
+	const playerColor = "black"; // colour of player tokens
+	const previewColor = "lightgray"; // colour of preview tokens
 
-	const checkForWin = () => {
-		// grabs the contents of the board
-		const board = [];
-		for (let i = 0; i < 9; i++) {
-			const square = document.getElementById(`${i}`);
-			board.push(square.textContent);
-		}
-		// checks for winning game patterns
-		// horizontal patterns
-		for (let i = 0; i < 8; i += 3) {
-			if (
-				board[i] !== "" &&
-				board[i] === board[i + 1] &&
-				board[i] === board[i + 2]
-			) {
-				endGame([i, i + 1, i + 2], board[i]);
-				return;
-			}
-		}
-		// vertical patterns
-		for (let i = 0; i < 3; i++) {
-			if (
-				board[i] !== "" &&
-				board[i] === board[i + 3] &&
-				board[i] === board[i + 6]
-			) {
-				endGame([i, i + 3, i + 6], board[i]);
-				return;
-			}
-		}
-		// diagonal patterns
-		if (board[0] !== "" && board[0] === board[4] && board[0] === board[8]) {
-			endGame([0, 4, 8], board[0]);
-			return;
-		} else if (
-			board[2] !== "" &&
-			board[2] === board[4] &&
-			board[2] === board[6]
-		) {
-			endGame([2, 4, 6], board[2]);
-			return;
-		} else if (filledSquares.length === 8) {
-			// if board is full and no winner, game over
-			endGame(false);
-		}
+	const getClassName = (i) => {
+		// used when generating grid to determine lines
+		const list = [];
+		if (i < 3) list.push(s.Board_Square__Top);
+		if (i === 4) list.push(s.Board_Square__Center);
+		if (i > 5) list.push(s.Board_Square__Bottom);
+		if (i % 3 === 0) list.push(s.Board_Square__Left);
+		if ((i + 1) % 3 === 0) list.push(s.Board_Square__Right);
+		return list.join(" ");
 	};
 
-	const endGame = (winningPattern, winner) => {
-		if (typeof winningPattern === "boolean") {
-			// game over scenario
-			for (let i = 0; i < 9; i++) {
-				const square = document.getElementById(`${i}`);
-				square.style.backgroundColor = loserColor;
+	const checkForWin = () => {
+		const checkPattern = (threeSquares) => {
+			// takes in the value of three squares and checks if they're a match
+			const joined = threeSquares.join("");
+			if (
+				joined === `${playerOne}${playerOne}${playerOne}` ||
+				joined === `${playerTwo}${playerTwo}${playerTwo}`
+			) {
+				endGame(threeSquares[0]);
+				return true;
 			}
-			setInfoText("Game Over! Draw!");
-		} else {
-			// color winning and losing tiles appropriately
-			for (let i = 0; i < 9; i++) {
-				const square = document.getElementById(`${i}`);
-				square.style.backgroundColor = winningPattern.includes(i)
-					? winnerColor
-					: loserColor;
-			}
-			setInfoText(`Victory! ${winner} wins!`);
+		};
+		// grab the contents of the board
+		const pattern = [];
+		for (let i = 0; i < 9; i++) {
+			const square = board.current.children[i];
+			pattern.push(square.textContent);
 		}
-		// pull up info
-		setInfoZIndex(1);
-		setInfoOpacity(1);
+		console.log(pattern);
+		// check for winning game patterns
+		// horizontal patterns
+		if (checkPattern([pattern[0], pattern[1], pattern[2]])) return;
+		if (checkPattern([pattern[3], pattern[4], pattern[5]])) return;
+		if (checkPattern([pattern[6], pattern[7], pattern[8]])) return;
+		// vertical patterns
+		if (checkPattern([pattern[0], pattern[3], pattern[6]])) return;
+		if (checkPattern([pattern[1], pattern[4], pattern[7]])) return;
+		if (checkPattern([pattern[2], pattern[5], pattern[8]])) return;
+		// diagonal patterns
+		if (checkPattern([pattern[0], pattern[4], pattern[8]])) return;
+		if (checkPattern([pattern[2], pattern[4], pattern[6]])) return;
+		// if board is full and no winner, game over
+		if (filledSquares.length === 8) endGame(false);
+	};
+
+	const endGame = (winner) => {
+		for (let i = 0; i < 9; i++) {
+			const square = board.current.children[i];
+			if (!winner) {
+				// Draw - all squares lose
+				square.style.backgroundColor = loserColor;
+			} else {
+				console.log(square.textContent);
+				// Color winner and loser squares appropriately
+				square.style.backgroundColor =
+					square.textContent === winner ? winnerColor : loserColor;
+			}
+		}
+		// set info content and pull up info
+		setInfoText(winner ? `Victory! ${winner} wins!` : "Game Over! Draw!");
+		setInfoState(1);
+	};
+
+	const resetSquare = (square) => {
+		square.textContent = "";
+		square.style.color = playerColor;
+		square.style.backgroundColor = "white";
 	};
 
 	const resetGame = () => {
 		for (let i = 0; i < 9; i++) {
 			// reset square colours
-			const square = document.getElementById(`${i}`);
-			square.textContent = "";
-			square.style.backgroundColor = "white";
+			const square = board.current.children[i];
+			resetSquare(square);
 		}
 		// reset board
-		setPreviewSquare("");
 		setFilledSquares([]);
 		// hide info
-		setInfoOpacity(0);
-		setInfoZIndex(-1);
+		setInfoState(-1);
 	};
 
 	const handleClick = (e) => {
 		const square = e.target;
+		const squareVal = square.getAttribute("data-value");
 		const input = document.createTextNode(
 			// alternate between players
 			filledSquares.length % 2 === 0 ? playerOne : playerTwo
 		);
-		if (!filledSquares.includes(square.id)) {
+		if (!filledSquares.includes(squareVal)) {
 			// remove preview
-			square.textContent = "";
-			square.style.color = playerColor;
+			resetSquare(square);
 			// place player
 			square.appendChild(input);
-			setFilledSquares(filledSquares.concat(square.id));
+			setFilledSquares(filledSquares.concat(squareVal));
 		}
 		checkForWin();
 	};
@@ -125,112 +116,64 @@ const TicTacToe = ({ size = 300 }) => {
 	const handleMouseEnter = (e) => {
 		// places preview icon in square on hover
 		const square = e.target;
-		const input = document.createTextNode(
-			filledSquares.length % 2 === 0 ? playerOne : playerTwo
-		);
-		if (!filledSquares.includes(square.id)) {
+		const squareVal = square.getAttribute("data-value");
+		if (!filledSquares.includes(squareVal)) {
+			const input = document.createTextNode(
+				filledSquares.length % 2 === 0 ? playerOne : playerTwo
+			);
 			square.style.color = previewColor;
 			square.appendChild(input);
-			setPreviewSquare(square.id);
 		}
 	};
 
 	const handleMouseLeave = (e) => {
 		// removes preview icon after leaving square
 		const square = e.target;
-		if (previewSquare === square.id && !filledSquares.includes(square.id)) {
-			square.style.color = playerColor;
-			square.textContent = "";
+		const squareVal = square.getAttribute("data-value");
+		if (!filledSquares.includes(squareVal)) {
+			resetSquare(square);
 		}
 	};
 
 	return (
 		<>
 			<div
-				className={styles.TTTInfo}
+				className={s.Info}
 				style={{
 					width: size,
 					height: size,
-					opacity: infoOpacity,
-					zIndex: infoZIndex,
+					opacity: infoState,
+					zIndex: infoState,
 				}}
 			>
-				<p className={styles.TTTInfo_Title}>Tic-Tac-Toe</p>
-				<p className={styles.TTTInfo_Content}>{infoText}</p>
-				<button
-					className={styles.TTTInfo_Button}
-					onClick={() => resetGame()}
-				>
+				<p className={s.Info_Title}>Tic-Tac-Toe</p>
+				<p className={s.Info_Content}>{infoText}</p>
+				<button className={s.Info_Button} onClick={resetGame}>
 					Play
 				</button>
 			</div>
 			<div
-				className={styles.TTTBoard}
+				ref={board}
+				className={s.Board}
 				style={{ width: size, height: size }}
 			>
-				<div
-					id="0"
-					className={`${styles.TTTBoard_Square__Top} ${styles.TTTBoard_Square__Left}`}
-					onClick={handleClick}
-					onMouseEnter={handleMouseEnter}
-					onMouseLeave={handleMouseLeave}
-				></div>
-				<div
-					id="1"
-					className={styles.TTTBoard_Square__Top}
-					onClick={handleClick}
-					onMouseEnter={handleMouseEnter}
-					onMouseLeave={handleMouseLeave}
-				></div>
-				<div
-					id="2"
-					className={`${styles.TTTBoard_Square__Top} ${styles.TTTBoard_Square__Right}`}
-					onClick={handleClick}
-					onMouseEnter={handleMouseEnter}
-					onMouseLeave={handleMouseLeave}
-				></div>
-				<div
-					id="3"
-					className={styles.TTTBoard_Square__Left}
-					onClick={handleClick}
-					onMouseEnter={handleMouseEnter}
-					onMouseLeave={handleMouseLeave}
-				></div>
-				<div
-					id="4"
-					className={styles.TTTBoard_Square__Center}
-					onClick={handleClick}
-					onMouseEnter={handleMouseEnter}
-					onMouseLeave={handleMouseLeave}
-				></div>
-				<div
-					id="5"
-					className={styles.TTTBoard_Square__Right}
-					onClick={handleClick}
-					onMouseEnter={handleMouseEnter}
-					onMouseLeave={handleMouseLeave}
-				></div>
-				<div
-					id="6"
-					className={`${styles.TTTBoard_Square__Bottom} ${styles.TTTBoard_Square__Left}`}
-					onClick={handleClick}
-					onMouseEnter={handleMouseEnter}
-					onMouseLeave={handleMouseLeave}
-				></div>
-				<div
-					id="7"
-					className={styles.TTTBoard_Square__Bottom}
-					onClick={handleClick}
-					onMouseEnter={handleMouseEnter}
-					onMouseLeave={handleMouseLeave}
-				></div>
-				<div
-					id="8"
-					className={`${styles.TTTBoard_Square__Bottom} ${styles.TTTBoard_Square__Right}`}
-					onClick={handleClick}
-					onMouseEnter={handleMouseEnter}
-					onMouseLeave={handleMouseLeave}
-				></div>
+				{Array.from(Array(9)).map((e, i) => {
+					return (
+						<svg
+							viewBox="10 10 50 50"
+							key={"square" + i}
+							data-value={i}
+							className={getClassName(i)}
+							onClick={handleClick}
+							onMouseEnter={handleMouseEnter}
+							onMouseLeave={handleMouseLeave}
+						>
+							<text x="20" y="20">
+								test
+							</text>
+						</svg>
+					);
+				})}
 			</div>
 		</>
 	);
